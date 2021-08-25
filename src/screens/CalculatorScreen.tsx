60dd84ed1396ab90952e2d343d4cac378c776d9c
César Gonzalez <cedgo1997@gmail.com>
2021-08-24 20:09:45 -0400

    Transition from total to subtotal in math funtions

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -1,13 +1,19 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Text, View } from 'react-native'
 import { CalcButton } from '../components/CalcButton'
 import { styles } from '../theme/appTheme'
 
+enum Operators {
+    sum, rest, multiply, divide
+}
+
 export const CalculatorScreen = () => {
 
-    const [total, setTotal] = useState('100');
-    const [subTotal, setSubTotal] = useState('100');
+    const [total, setTotal] = useState('0');
+    const [subTotal, setSubTotal] = useState('0');
+
+    const lastOperation = useRef<Operators>();
 
     const clear = () => {
         setTotal('0');
@@ -43,33 +49,59 @@ export const CalculatorScreen = () => {
         }
     };
 
+    const passToSubtotal = () => {
+        if (total.endsWith('.')) {
+            setSubTotal(total.slice(0, -1));
+        } else {
+            setSubTotal(total);
+        }
+        setTotal('0');
+    };
+
+    const btnDivide = () => {
+        passToSubtotal();
+        lastOperation.current = Operators.divide;
+    };
+    const btnMultiply = () => {
+        passToSubtotal();
+        lastOperation.current = Operators.multiply;
+    };
+    const btnRest = () => {
+        passToSubtotal();
+        lastOperation.current = Operators.rest;
+    };
+    const btnSum = () => {
+        passToSubtotal();
+        lastOperation.current = Operators.sum;
+    };
+
     return (
         <View style={styles.calculatorContainer}>
-            <Text style={styles.tinyResult}>{total !== '0' && subTotal}</Text>
+            <Text style={styles.tinyResult}>{!(subTotal === '0' && total === '0') && subTotal}</Text>
             <Text style={styles.result} numberOfLines={1} adjustsFontSizeToFit>{total}</Text>
             <View style={styles.row}>
                 <CalcButton text='C' color='#9B9B9B' action={clear} />
                 <CalcButton text='+/-' color='#9B9B9B' action={changeSign} />
                 <CalcButton text='del' color='#9B9B9B' action={deleteNum} />
-                <CalcButton text='/' color='#FF9427' />
+                <CalcButton text='/' color='#FF9427' action={btnDivide} />
             </View>
             <View style={styles.row}>
                 <CalcButton text='7' action={buildNumber} />
                 <CalcButton text='8' action={buildNumber} />
                 <CalcButton text='9' action={buildNumber} />
-                <CalcButton text='X' color='#FF9427' />
+                <CalcButton text='X' color='#FF9427' action={btnMultiply} />
             </View>
             <View style={styles.row}>
                 <CalcButton text='4' action={buildNumber} />
                 <CalcButton text='5' action={buildNumber} />
                 <CalcButton text='6' action={buildNumber} />
-                <CalcButton text='-' color='#FF9427' />
+                <CalcButton text='-' color='#FF9427' action={btnRest} />
             </View>
             <View style={styles.row}>
                 <CalcButton text='1' action={buildNumber} />
                 <CalcButton text='2' action={buildNumber} />
                 <CalcButton text='3' action={buildNumber} />
-                <CalcButton text='+' color='#FF9427' />
+                <CalcButton text='+' color='#FF9427' action={btnSum} />
             </View>
             <View style={styles.row}>
                 <CalcButton text='0' isBig action={buildNumber} />
